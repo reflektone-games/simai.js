2d2b828a1974cdfdc68b0833cac63c65c00d36de
xhayper <hayper1919@gmail.com>
2023-11-05 12:08:29 +0700

    feat: export all structures and enum

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 export { SimaiConvert } from "./simaiConvert";
+export { SimaiFile } from "./simaiFile";
+
+/* Export enum and structures */
+
+export { EachStyle } from "./structures/eachStyle";
+export { Location } from "./structures/location";
+export { MaiChart } from "./structures/maiChart";
+export { Note } from "./structures/note";
 export { NoteAppearance } from "./structures/noteAppearance";
-export { SlideMorph } from "./structures/slideMorph";
+export { NoteCollection } from "./structures/noteCollection";
+export { NoteGroup } from "./structures/noteGroup";
 export { NoteStyles } from "./structures/noteStyles";
 export { NoteType } from "./structures/noteType";
-export { Note } from "./structures/note";
-export { NoteCollection } from "./structures/noteCollection";
-export { SimaiFile } from "./simaiFile";
-export { MaiChart } from "./structures/maiChart";
+export { SlideMorph } from "./structures/slideMorph";
+export { SlidePath } from "./structures/slidePath";
+export { SlideSegment } from "./structures/slideSegment";
+export { SlideType } from "./structures/slideType";
